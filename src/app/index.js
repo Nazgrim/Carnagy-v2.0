e45d16dcb45695eccc285544bc9b84b12c99266c
Nazgrim <serega19911991@ya.ru>
2016-07-13 18:48:33 +0300

    refactoring

--- src/app/index.js
+++ src/app/index.js
@@ -9,51 +9,26 @@ import CarCtrl from './dealers/car.controller';
 
 angular.module('carnagy', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngResource',
     'ui.router', 'ui.bootstrap', 'uiGmapgoogle-maps', 'ui.select', 'ngSanitize', 'highcharts-ng', 'hightChartService', 'dealerModule'])
-    .controller('MainCtrl', MainCtrl)
     .controller('NavbarCtrl', NavbarCtrl)
     .controller('SidebarCtrl', SidebarCtrl)
-    .controller('DealersCtrl', DealersCtrl)
     .controller('DealerCtrl', DealerCtrl)
     .controller('CarCtrl', CarCtrl)
-    .config(function ($stateProvider, $urlRouterProvider, uiGmapGoogleMapApiProvider) {
-        uiGmapGoogleMapApiProvider.configure({
-            //    key: 'your api key',
-            v: '3.17',
-            libraries: 'weather,geometry,visualization'
-        });
+    .config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider
-            .state('main', {
-                url: '/',
-                templateUrl: 'app/main/main.html',
-                controller: 'MainCtrl'
-            })
             .state('car', {
                 url: '/car',
                 templateUrl: 'app/dealers/car.html',
                 controller: 'CarCtrl',
             })
-            .state('dealers', {
-                url: '/dealers',
-                resolve: {
-                    dealers: function (dealerService) {
-                        return dealerService.getDealers();
-                    },
-                    provinces: function (dealerService) {
-                        return dealerService.getProvinces();
-                    }
-                },
-                templateUrl: 'app/dealers/dealers.html',
-                controller: 'DealersCtrl',
-            })
             .state('dealer', {
-                url: '/dealer/:id',
+                url: '/dealer',
                 resolve: {
                     dealer: function ($stateParams, dealerService) {
-                        return dealerService.getDealerById($stateParams.id);
+                        return dealerService.getDealerById();
                     },
                 },
                 templateUrl: 'app/dealers/dealer.html',
                 controller: 'DealerCtrl',
             });
-        $urlRouterProvider.otherwise('/dealers');
+        $urlRouterProvider.otherwise('/dealer');
     });
