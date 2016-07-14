4890c86956d20b846287ecdb0aae9e3176f4eb46
Nazgrim <serega19911991@ya.ru>
2016-07-14 18:23:57 +0300

    add repeat

--- src/app/directives/similar-car/similarCar.js
+++ src/app/directives/similar-car/similarCar.js
@@ -4,5 +4,31 @@ angular
     return {
       restrict: 'E',
       templateUrl: '/app/directives/similar-car/similar-car.html',
+      link: function (scope, element, attrs) {
+        scope.cars = [
+          {
+            name: "Car 1",
+            imgUrl: "../app/img/car1.png",
+            maker: "Audi",
+            model: "TT",
+            drivetrain: "RWD",
+            price: 21000,
+          }, {
+            name: "Car 2",
+            imgUrl: "../app/img/car2.png",
+            maker: "Audi",
+            model: "TT",
+            drivetrain: "RWD",
+            price: 21000,
+          }, {
+            name: "Car 3",
+            imgUrl: "../app/img/car3.png",
+            maker: "Audi",
+            model: "TT",
+            drivetrain: "RWD",
+            price: 21000,
+          }
+        ];
+      }
     };
   });;
