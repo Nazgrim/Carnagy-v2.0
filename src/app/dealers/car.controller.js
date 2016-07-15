7abbe183cf075c84ac176a89a261e2b0d64a691a
Nazgrim <serega19911991@ya.ru>
2016-07-15 17:47:02 +0300

    add broadcast between directives

--- src/app/dealers/car.controller.js
+++ src/app/dealers/car.controller.js
@@ -1,11 +1,39 @@
 'use strict';
 class CarCtrl {
     constructor($scope, highchartsNG) {
-        $scope.car = {
-            price: 60000,
-            marketPrice: 70000,
-            different: 10000
-        };
+        $scope.cars = [
+            {
+                name: "Car 1",
+                imgUrl: "../app/img/car1.png",
+                maker: "Audi",
+                model: "TT",
+                drivetrain: "RWD",
+                price: 21000,
+            }, {
+                name: "Car 2",
+                imgUrl: "../app/img/car2.png",
+                maker: "Audi",
+                model: "TT",
+                drivetrain: "RWD",
+                price: 21000,
+            }, {
+                name: "Car 3",
+                imgUrl: "../app/img/car3.png",
+                maker: "Audi",
+                model: "TT",
+                drivetrain: "RWD",
+                price: 21000,
+                
+            }
+        ];
+        $scope.$on("addToCompareUpEvent", function (event, data) {
+            console.log("вверх");
+            $scope.$broadcast('addToCompareDownEvent', data);
+        });
+        $scope.$on("removeToCompareUpEvent", function (event, data) {
+            console.log("вверх");
+            $scope.$broadcast('removeToCompareDownEvent', data);
+        });
     }
 }
 
--- src/app/directives/similar-car/similarCar.js
+++ src/app/directives/similar-car/similarCar.js
@@ -5,30 +5,36 @@ angular
       restrict: 'E',
       templateUrl: '/app/directives/similar-car/similar-car.html',
       link: function (scope, element, attrs) {
-        scope.cars = [
-          {
-            name: "Car 1",
-            imgUrl: "../app/img/car1.png",
-            maker: "Audi",
-            model: "TT",
-            drivetrain: "RWD",
-            price: 21000,
-          }, {
-            name: "Car 2",
-            imgUrl: "../app/img/car2.png",
-            maker: "Audi",
-            model: "TT",
-            drivetrain: "RWD",
-            price: 21000,
-          }, {
-            name: "Car 3",
-            imgUrl: "../app/img/car3.png",
-            maker: "Audi",
-            model: "TT",
-            drivetrain: "RWD",
-            price: 21000,
+        scope.removeToCompare = function (car, index) {
+          car.isAdd = false;
+          scope.$emit("removeToCompareUpEvent", index);
+        };
+        scope.addToCompare = function (car, index) {
+          car.isAdd = true;
+          var data;
+          switch (index) {
+            case 0:
+              data = {
+                name: 'Car 1',
+                data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+              };
+              break;
+            case 1:
+              data = {
+                name: 'Car 2',
+                data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+              };
+              break;
+            case 2:
+              data = {
+                name: 'Car 3',
+                data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+              }
+              break;
           }
-        ];
+          scope.$emit("addToCompareUpEvent", data);
+          
+        }
       }
     };
   });;
