b03b62b940f7da6e26b6bf099e65e86c535f4697
Smirnov Dmitriy <serega19911991@ya.ru>
2016-07-23 21:41:03 +0300

    complete car page

--- src/app/dealers/car.controller.js
+++ src/app/dealers/car.controller.js
@@ -1,18 +1,73 @@
 'use strict';
 class CarCtrl {
-    constructor($scope, carService, id) {
+    constructor($scope, carService, id, highchartsNG) {
         $scope.car = carService.getCarById(id);
         $scope.$on("addToCompareUpEvent", function (event, data) {
             console.log("вверх");
-            $scope.$broadcast('addToCompareDownEvent', data);
+            //$scope.$broadcast('addToCompareDownEvent', data);
+            $scope.chartConfig1.series.push(data);
         });
         $scope.$on("removeToCompareUpEvent", function (event, data) {
             console.log("вверх");
-            $scope.$broadcast('removeToCompareDownEvent', data);
+            for (var i = 0; i < $scope.chartConfig1.series.length; i++) {
+                if ($scope.chartConfig1.series[i].carId == data) {
+                    $scope.chartConfig1.series.splice(i, 1);
+                    break;
+                }
+            }
+            //$scope.$broadcast('removeToCompareDownEvent', data);
         });
+        $scope.chartConfig1 = getPriceTrendChart();
+
+        function getPriceTrendChart() {
+            return {
+                options: {
+                    chart: {
+                        type: 'line',
+                        height: 300
+                    },
+                    legend: {
+                        enabled: true
+                    },
+                    exporting: {
+                        enabled: false
+                    }
+                },
+                title: {
+                    text: '',
+                    style: {
+                        display: 'none'
+                    }
+                },
+                subtitle: {
+                    text: '',
+                    style: {
+                        display: 'none'
+                    }
+                },
+                series: [
+                    {
+                        name: 'Ford Fusion',
+                        data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+                    }
+                ],
+                xAxis: {
+                    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                },
+                yAxis: {
+                    title: {
+                        text: '',
+                        style: {
+                            display: 'none'
+                        }
+                    }
+                },
+
+            };
+        }
     }
 }
 
-CarCtrl.$inject = ['$scope', 'carService', 'id'];
+CarCtrl.$inject = ['$scope', 'carService', 'id', 'highchartsNG'];
 
 export default CarCtrl;
--- src/app/directives/car-chart/carChart.js
+++ src/app/directives/car-chart/carChart.js
@@ -4,27 +4,45 @@ angular
         return {
             restrict: 'E',
             templateUrl: '/app/directives/car-chart/car-chart.html',
-            link: function (scope, element, attrs) {
-                scope.$on("addToCompareDownEvent", function (event, data) {
-                    scope.chartConfig.series.push(data);
+            controller: function ($scope) {
+                $scope.chartConfig = getchartConfig1();
+                $scope.$on("addToCompareDownEvent", function (event, data) {
+                    $scope.chartConfig.series.push(data);
                 })
-                scope.$on("removeToCompareDownEvent", function (event, data) {
-                    for (var i = 0; i < scope.chartConfig.series.length; i++) {
-                        if (scope.chartConfig.series[i].carId == data) {
-                            scope.chartConfig.series.splice(i, 1);
+                $scope.$on("removeToCompareDownEvent", function (event, data) {
+                    for (var i = 0; i < $scope.chartConfig.series.length; i++) {
+                        if ($scope.chartConfig.series[i].carId == data) {
+                            $scope.chartConfig.series.splice(i, 1);
                             break;
                         }
                     }
                 })
-                scope.chartConfig = getchartConfig();
-                scope.chartConfig.series = [scope.car.chartSeries];
-            }
+            },
+            // link: function (scope, element, attrs) {
+
+
+            // }
         }
-        function getchartConfig() {
+        function getchartConfig1() {
             return {
-                options: {
+                "options": {
+                    plotOptions: {
+                        bar: {
+                            borderWidth: 0,
+                            dataLabels: {
+                                enabled: true,
+                                allowOverlap: true,
+                            }
+                        },
+                        series: {
+                            borderWidth: 0,
+                            dataLabels: {
+                                enabled: true
+                            }
+                        }
+                    },
                     chart: {
-                        type: 'line',
+                        type: 'column',
                         height: 300
                     },
                     legend: {
@@ -32,7 +50,33 @@ angular
                     },
                     exporting: {
                         enabled: false
-                    }
+                    },
+                    // plotOptions: {
+                    //     column: {
+                    //         states: {
+                    //             hover: {
+                    //                 color: '#000000'
+                    //             }
+                    //         }
+                    //     },
+                    //     series: {
+                    //         allowPointSelect: true,
+                    //         cursor: 'pointer',
+                    //         point: {
+                    //             events: {
+                    //                 click: function () {
+                    //                     $("#selectedSeries").text('Category: ' + this.category + ', value: ' + this.y);
+                    //                 },
+                    //                 mouseOver: function () {
+                    //                     $("#focusedSeries").text('Category: ' + this.category + ', value: ' + this.y);
+                    //                 },
+                    //                 mouseOut: function () {
+                    //                     $("#focusedSeries").text('');
+                    //                 }
+                    //             }
+                    //         }
+                    //     }
+                    // }
                 },
                 title: {
                     text: '',
@@ -40,23 +84,84 @@ angular
                         display: 'none'
                     }
                 },
+                series: [
+                    {
+                        name: 'Market',
+                        type: 'column',
+                        data: [
+                            { x: 0, "y": 49.9 },
+                            { x: 1, "y": 71.5 },
+                            { x: 2, "y": 106.4 },
+                            { x: 3, "y": 129.2 },
+                            { x: 4, "y": 144 },
+                            { x: 5, "y": 176 },
+                            { x: 6, "y": 135.6, color: "#00ff00" },
+                            { x: 7, "y": 148.5 },
+                            { x: 8, "y": 216.4 },
+                            { x: 9, "y": 194.1 },
+                            { x: 10, "y": 95.6 },
+                            { x: 11, "y": 54.4 }
+                        ],
+                        tooltip: {
+                            valueSuffix: ' count'
+                        }
+                        // ,
+                        // zoneAxis: 'x',
+                        // zones: [{
+                        //     value: 2.5,
+                        //     color: '#f7a35c'
+                        // }, {
+                        //         value: 5,
+                        //         color: '#7cb5ec'
+                        //     }, {
+                        //         value: 10,
+                        //         color: '#90ed7d'
+                        //     }, {
+                        //         color: '#f7a35c'
+                        //     }]
+                    }
+                    //Variant 2
+                    ,
+                    {
+                        name: 'Delaer',
+                        type: 'column',
+                        color: '#ff0000',
+                        data:
+                        [
+                            { x: 6, "y": 135.6 }
+                        ]
+                    }
+                ],
+                yAxis: {
+                    title: {
+                        text: '',
+                        style: 'display:none'
+                    }
+                },
+                xAxis: {
+
+                    type: 'category',
+                    categories: [
+                        '12.200 $',
+                        '13.200 $',
+                        '14.200 $',
+                        '15.200 $',
+                        '16.200 $',
+                        '17.200 $',
+                        '18.200 $',
+                        '19.200 $',
+                        '20.200 $',
+                        '21.200 $',
+                        '22.200 $',
+                        '23.200 $'
+                    ]
+                },
                 subtitle: {
                     text: '',
                     style: {
                         display: 'none'
                     }
-                },
-                xAxis: {
-                    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-                },
-                yAxis: {
-                    title: {
-                        text: '',
-                        style: {
-                            display: 'none'
-                        }
-                    }
-                },
+                }
 
             };
         }
