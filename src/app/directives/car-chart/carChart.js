211f7e83377c7f0ee27db3659557b800d90bb31f
Nazgrim <serega19911991@ya.ru>
2016-07-30 14:04:43 +0300

    completed work on car chart

--- src/app/directives/car-chart/carChart.js
+++ src/app/directives/car-chart/carChart.js
@@ -5,7 +5,135 @@ angular
             restrict: 'E',
             templateUrl: '/app/directives/car-chart/car-chart.html',
             controller: function ($scope) {
-                $scope.chartConfig = getchartConfig1();
+
+                var chartConfig = getchartConfig1();
+                chartConfig.xAxis.plotBands = [
+                    {
+                        chartValue: 'below',
+                        color: '#FCFFC5',
+                        from: -1.5,
+                        to: 1.5,
+                        label: {
+                            text: '<div><b>Exceptional Price</b></div><div>Less than 16,200$</div>',
+                            align: 'left',
+                            useHTML: true,
+                        },
+                        events: {
+                            mouseover: function (e) {
+                                $scope.selectedLegend.currentBandSelected = this.options.chartValue;
+                                $scope.$apply();
+                            },
+                            mouseout: function (e) {
+                                $scope.selectedLegend.currentBandSelected = $scope.selectedLegend.bandSelected;
+                                $scope.$apply();
+                            }
+                        }
+                    }, {
+                        chartValue: 'great',
+                        color: '#c4c336',
+                        from: 1.5,
+                        to: 4.5,
+                        label: {
+                            text: '<div><b>Great Price</b></div><div>Less than 21,200$</div>',
+                            align: 'left',
+                            useHTML: true,
+                        },
+                        events: {
+                            mouseover: function (e) {
+                                $scope.selectedLegend.currentBandSelected = this.options.chartValue;
+                                $scope.$apply();
+                            },
+                            mouseout: function (e) {
+                                $scope.selectedLegend.currentBandSelected = $scope.selectedLegend.bandSelected;
+                                $scope.$apply();
+                            }
+                        }
+                    }, {
+                        chartValue: 'good',
+                        color: '#CCFFC5',
+                        from: 4.5,
+                        to: 7.5,
+                        label: {
+                            text: '<div><b>Good price</b></div><div>Less than 21,200$</div>',
+                            align: 'left',
+                            useHTML: true,
+                        },
+                        events: {
+                            mouseover: function (e) {
+                                $scope.selectedLegend.currentBandSelected = this.options.chartValue;
+                                $scope.$apply();
+                            },
+                            mouseout: function (e) {
+                                $scope.selectedLegend.currentBandSelected = $scope.selectedLegend.bandSelected;
+                                $scope.$apply();
+                            }
+                        }
+                    }, {
+                        chartValue: 'above',
+                        color: '#4ac336',
+                        from: 7.5,
+                        to: 11.5,
+                        label: {
+                            text: '<div><b>Above Market</b></div><div>Less than 21,200$ or more</div>',
+                            align: 'left',
+                            useHTML: true,
+                        },
+                        events: {
+                            mouseover: function (e) {
+                                $scope.selectedLegend.currentBandSelected = this.options.chartValue;
+                                $scope.$apply();
+                            },
+                            mouseout: function (e) {
+                                $scope.selectedLegend.currentBandSelected = $scope.selectedLegend.bandSelected;
+                                $scope.$apply();
+                            }
+                        }
+                    },
+                ];
+
+                chartConfig.options.plotOptions.series.point = {
+                    events: {
+                        mouseOver: function (e) {
+                            //e.stopPropagation();
+                            $scope.selectedLegend.currentSelected = 'series';
+                            $scope.$apply();
+                        },
+                        mouseOut: function (e) {
+                            //e.stopPropagation();
+                            $scope.selectedLegend.currentSelected = $scope.selectedLegend.selected;
+                            $scope.$apply();
+                        }
+                    }
+                };
+                chartConfig.series = [
+                    {
+                        name: 'Market',
+                        type: 'column',
+                        data: [
+                            { x: 0, "y": 49.9 },
+                            { x: 1, "y": 71.5 },
+                            { x: 2, "y": 106.4 },
+                            { x: 3, "y": 129.2 },
+                            { x: 4, "y": 144 },
+                            { x: 5, "y": 176 },
+                            {
+                                x: 6,
+                                y: 150
+
+                            },
+                            { x: 7, "y": 148.5 },
+                            { x: 8, "y": 216.4 },
+                            { x: 9, "y": 194.1 },
+                            { x: 10, "y": 95.6 },
+                            { x: 11, "y": 54.4 }
+                        ],
+                        tooltip: {
+                            valueSuffix: ' count'
+                        }
+                    }
+                ];
+                $scope.chartConfig = chartConfig;
+                //{ x: 6, "y": 135.6 },
                 $scope.$on("addToCompareDownEvent", function (event, data) {
                     $scope.chartConfig.series.push(data);
                 })
@@ -16,12 +144,18 @@ angular
                             break;
                         }
                     }
-                })
-            },
-            // link: function (scope, element, attrs) {
+                });
+                $scope.selectedLegend = {
+                    selected: "bands",
+                    currentSelected: "bands",
+                    bandSelected: "below",
+                    currentBandSelected: "below",
+                    people: 10,
+                    from: '$30,936',
+                    to: '$31,248'
+                };
 
-
-            // }
+            }
         }
         function getchartConfig1() {
             return {
@@ -30,14 +164,36 @@ angular
                         bar: {
                             borderWidth: 0,
                             dataLabels: {
-                                enabled: true,
-                                allowOverlap: true,
+                                enabled: false,
+                                allowOverlap: false,
                             }
                         },
                         series: {
                             borderWidth: 0,
                             dataLabels: {
-                                enabled: true
+                                enabled: false
+                            },
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            // point: {
+                            //     events: {
+                            //         click: function () {
+                            //             $("#selectedSeries").text('Category: ' + this.category + ', value: ' + this.y);
+                            //         },
+                            //         mouseOver: function () {
+                            //             $("#focusedSeries").text('Category: ' + this.category + ', value: ' + this.y);
+                            //         },
+                            //         mouseOut: function () {
+                            //             $("#focusedSeries").text('');
+                            //         }
+                            //     }
+                            // }
+                        },
+                        column: {
+                            states: {
+                                hover: {
+                                    color: '#000000'
+                                }
                             }
                         }
                     },
@@ -52,31 +208,7 @@ angular
                         enabled: false
                     },
                     // plotOptions: {
-                    //     column: {
-                    //         states: {
-                    //             hover: {
-                    //                 color: '#000000'
-                    //             }
-                    //         }
-                    //     },
-                    //     series: {
-                    //         allowPointSelect: true,
-                    //         cursor: 'pointer',
-                    //         point: {
-                    //             events: {
-                    //                 click: function () {
-                    //                     $("#selectedSeries").text('Category: ' + this.category + ', value: ' + this.y);
-                    //                 },
-                    //                 mouseOver: function () {
-                    //                     $("#focusedSeries").text('Category: ' + this.category + ', value: ' + this.y);
-                    //                 },
-                    //                 mouseOut: function () {
-                    //                     $("#focusedSeries").text('');
-                    //                 }
-                    //             }
-                    //         }
-                    //     }
-                    // }
+
                 },
                 title: {
                     text: '',
@@ -84,54 +216,7 @@ angular
                         display: 'none'
                     }
                 },
-                series: [
-                    {
-                        name: 'Market',
-                        type: 'column',
-                        data: [
-                            { x: 0, "y": 49.9 },
-                            { x: 1, "y": 71.5 },
-                            { x: 2, "y": 106.4 },
-                            { x: 3, "y": 129.2 },
-                            { x: 4, "y": 144 },
-                            { x: 5, "y": 176 },
-                            { x: 6, "y": 135.6, color: "#00ff00" },
-                            { x: 7, "y": 148.5 },
-                            { x: 8, "y": 216.4 },
-                            { x: 9, "y": 194.1 },
-                            { x: 10, "y": 95.6 },
-                            { x: 11, "y": 54.4 }
-                        ],
-                        tooltip: {
-                            valueSuffix: ' count'
-                        }
-                        // ,
-                        // zoneAxis: 'x',
-                        // zones: [{
-                        //     value: 2.5,
-                        //     color: '#f7a35c'
-                        // }, {
-                        //         value: 5,
-                        //         color: '#7cb5ec'
-                        //     }, {
-                        //         value: 10,
-                        //         color: '#90ed7d'
-                        //     }, {
-                        //         color: '#f7a35c'
-                        //     }]
-                    }
-                    //Variant 2
-                    ,
-                    {
-                        name: 'Delaer',
-                        type: 'column',
-                        color: '#ff0000',
-                        data:
-                        [
-                            { x: 6, "y": 135.6 }
-                        ]
-                    }
-                ],
+                series: [],
                 yAxis: {
                     title: {
                         text: '',
@@ -139,6 +224,41 @@ angular
                     }
                 },
                 xAxis: {
+                    plotLines: [
+                        {
+                            color: '#FF0000',
+                            width: 2,
+                            value: 5.5,
+                            label: {
+                                text: 'Your price',
+                                verticalAlign: 'middle',
+                                textAlign: 'center'
+                            },
+                            zIndex: 5
+                        },
+                        {
+                            color: '#00FF00',
+                            width: 2,
+                            value: 5.0,
+                            label: {
+                                text: 'Avr. price',
+                                verticalAlign: 'middle',
+                                textAlign: 'center'
+                            },
+                            zIndex: 5
+                        },
+                        {
+                            color: '#0000FF',
+                            width: 2,
+                            value: 0.5,
+                            label: {
+                                text: 'MSRP price',
+                                verticalAlign: 'middle',
+                                textAlign: 'center'
+                            },
+                            zIndex: 5
+                        }
+                    ],
 
                     type: 'category',
                     categories: [
